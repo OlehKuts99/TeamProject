185efb4ace10a405b79dac474c2debf03440321b
aasandroqq <aasandroqq@gmail.com>
2019-03-16 14:25:53 +0200

    Producer model has been added

---
+++ StoreApp/Models/Producer.cs
@@ -0,0 +1,19 @@
+﻿namespace StoreApp.Models
+{
+    /// <summary>
+    /// Producer model for create entity in database
+    /// </summary>
+    public class Producer
+    {
+        public int ProducerId { get; set; }
+
+        public string Name { get; set; }
+
+        public int Phone { get; set; }
+
+        public string Email { get; set; }
+
+        public string WebSite { get; set; }
+
+    }
+}
