246cbc3d1507fe531cf61ff4de63dbd0b59b2dc1
IvanPilyak <pilyak.vanya@gmail.com>
2019-03-16 15:49:17 +0200

    Added Xml documentation to Order model.

--- StoreApp/Models/Order.cs
+++ StoreApp/Models/Order.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 
 namespace StoreApp.Models
 {
+    /// <summary>
+    /// Order model for create entity in database
+    /// </summary>
     public class Order
     {
         public int Id { get; set; }
