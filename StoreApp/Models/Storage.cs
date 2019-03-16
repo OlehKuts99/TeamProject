1db59e060d9a026f6947a06aaeb214a54f526ec4
Ostap Khvostyk <ostaphvostyk@gmail.com>
2019-03-16 16:10:36 +0200

    Add Storage model

--- StoreApp/Models/Storage.cs
+++ StoreApp/Models/Storage.cs
@@ -2,11 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+///
 namespace StoreApp.Models
 {
+    /// <summary>
+    /// Storage model for create entity in database
+    /// </summary>
     public class Storage
     {
+        public int Id { get; set; }
+
+        public string City { get; set; }
+
+        public string Street { get; set; }
+
+        public ICollection<Good> Products { get; set; }
 
     }
 }
