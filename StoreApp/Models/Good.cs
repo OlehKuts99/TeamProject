1c4b3e09cd1fd2d0661e251618f10f44ddf9aee9
Andriy Galas <halasandreyko@gmail.com>
2019-03-16 15:28:04 +0200

    Add class Good

---
+++ StoreApp/Models/Good.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Runtime.CompilerServices;
+
+namespace StoreApp.Models
+{
+    /// <summary>
+    /// Good model for create entity in database
+    /// </summary>
+    public class Good
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Specification { get; set; }
+
+        public string PhotoUrl { get; set; }
+
+        public int YearOfManufacture { get; set; }
+
+        public int WarrantyTerm { get; set; }
+
+        public int ProducerId { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Type { get; set; }
+
+        public int Count { get; set; }
+
+        public ICollection Orders { get; set; }
+
+        public ICollection Storages { get; set; }
+    }
+}
