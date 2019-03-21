0586fd5b2534bf94ddb38ae7d2520e7da0c2ea7f
Oleh Kuts <oleh.kuts99@gmail.com>
2019-03-21 12:39:22 +0200

    Added Register method to AccountController also added RegisterViewModel

--- Store/Controllers/AccountController.cs
+++ Store/Controllers/AccountController.cs
@@ -1,8 +1,82 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Store.Models;
+using Store.ViewModels;
 
 namespace Store.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly SignInManager<ApplicationUser> signInManager;
+        private readonly AppDbContext appContext;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager,
+            AppDbContext appDbContext)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+            this.appContext = appDbContext;
+        }
+
+        /// <summary>
+        /// Get method to show registration form.
+        /// </summary>
+        /// <returns>Register view that contains registration form.</returns>
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Post method that will be runned after pushing on the button in registration form. 
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns>If operation is success it redirects to main page, otherwise it will return the same page.</returns>
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterView model)
+        {
+            if (ModelState.IsValid)
+            {
+                ApplicationUser user = new ApplicationUser
+                {
+                    Email = model.Email,
+                    UserName = string.Join(' ', model.FirstName, model.SecondName),
+                    CreateDate = DateTime.Now,
+                    UpdateTime = DateTime.Now
+                };
+
+                Customer customer = new Customer
+                {
+                    FirstName = model.FirstName,
+                    SecondName = model.SecondName,
+                    Phone = model.Phone,
+                    Email = model.Email
+                };
+
+                var addResult = await userManager.CreateAsync(user, model.Password);
+
+                if (addResult.Succeeded)
+                {
+                    await signInManager.SignInAsync(user, false);
+                    await appContext.Customers.AddAsync(customer);
+                    await appContext.SaveChangesAsync();
+
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach (var error in addResult.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+            }
+
+            return View(model);
+        }
     }
 }
---
+++ Store/ViewModels/RegisterView.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Store.ViewModels
+{
+    public class RegisterView
+    {
+        [Required]
+        [Display(Name = "FirstName")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "SecondName")]
+        public string SecondName { get; set; }
+
+        [Required]
+        [Display(Name = "Phone")]
+        public int Phone { get; set; }
+
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password", ErrorMessage = "Passwords don't match!")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        public string PasswordConfirm { get; set; }
+    }
+}
