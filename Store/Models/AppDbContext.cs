c2faa57537790c6d45af666e6ea0a74fa6589150
Oleh Kuts <oleh.kuts99@gmail.com>
2019-03-21 12:05:32 +0200

    Added model for user's accounts and database context

---
+++ Store/Models/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Store.Models
+{
+    /// <summary>
+    /// Context for database, here is current state of tables in database.
+    /// </summary>
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Good> Goods { get; set; }
+        public DbSet<Producer> Producers { get; set; }
+        public DbSet<Storage> Storages { get; set; }
+    }
+}
