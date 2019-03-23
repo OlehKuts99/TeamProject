c942a402765947d363dcaf6eded0dd619933bc05
Oleh Kuts <oleh.kuts99@gmail.com>
2019-03-23 15:09:08 +0200

    Added initalizer for database, now is avaliable admin account

---
+++ Store/Classes/DataBaseInitializer.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Store.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Store.Classes
+{
+    public class DataBaseInitializer
+    {
+        public static async Task InitializeAsync(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            string adminEmail = "administrator123@gmail.com";
+            string adminPassword = "Admin123";
+
+            if (await roleManager.FindByNameAsync("admin") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("admin"));
+            }
+
+            if (await roleManager.FindByNameAsync("customer") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("customer"));
+            }
+
+            if (await userManager.FindByNameAsync(adminEmail) == null)
+            {
+                ApplicationUser admin = new ApplicationUser { Email = adminEmail, UserName = adminEmail };
+                IdentityResult result = await userManager.CreateAsync(admin, adminPassword);
+
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(admin, "admin");
+                }
+            }
+        }
+    }
+}
