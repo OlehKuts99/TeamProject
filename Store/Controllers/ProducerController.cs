cf796c1ba74d67302bd21fcf34aabab4fbd88c11
Alexander Antonyuk <aasandroqq@gmail.com>
2019-03-26 22:33:25 +0200

    Producer CRUD

---
+++ Store/Controllers/ProducerController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Store.Classes;
+using Store.Classes.UnitOfWork;
+using Store.Models;
+using Store.ViewModels;
+
+namespace Store.Controllers
+{
+    public class ProducerController : Controller
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly SignInManager<ApplicationUser> signInManager;
+        private readonly RoleManager<IdentityRole> roleManager;
+        private readonly UnitOfWork unitOfWork;
+
+        public ProducerController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager,
+            RoleManager<IdentityRole> roleManager, AppDbContext appDbContext)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+            this.roleManager = roleManager;
+            this.unitOfWork = new UnitOfWork(appDbContext);
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(unitOfWork.Producers.GetAll().ToList());
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public  async Task<IActionResult> Create(CreateProducerView model)
+        {
+            if (ModelState.IsValid)
+            {
+                Producer producer = new Producer
+                {
+                    Name = model.Name,
+                    Phone = model.Phone,
+                    Email = model.Email,
+                    WebSite = model.WebSite
+                };
+                await unitOfWork.Producers.Create(producer);
+                await unitOfWork.SaveAsync();
+
+                return RedirectToAction("Index", "Producer");
+
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            Producer producer = await unitOfWork.Producers.Get(id);
+           
+            if (producer == null)
+            {
+                return NotFound();
+            }
+
+            EditProducerView model = new EditProducerView
+            {
+                Id = producer.Id,
+                Name = producer.Name,
+                Phone = producer.Phone,
+                Email = producer.Email,
+                WebSite = producer.WebSite
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(EditProducerView model)
+        {
+            if (ModelState.IsValid)
+            {
+                Producer producer = await unitOfWork.Producers.Get(model.Id);
+                
+                if (producer != null)
+                {
+                    producer.Name = model.Name;
+                    producer.Phone = model.Phone;
+                    producer.Email = model.Email;
+                    producer.WebSite = model.WebSite;
+                    unitOfWork.Producers.Update(producer);
+                    await unitOfWork.SaveAsync();
+
+                }
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Delete(int id)
+        {
+            Producer producer = await unitOfWork.Producers.Get(id);
+            if (producer != null)
+            {
+                await unitOfWork.Producers.Delete(id);
+                await unitOfWork.SaveAsync();
+            }
+
+            return RedirectToAction("Index");
+        }
+        
+    }
+}
---
+++ Store/ViewModels/EditProducerView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Store.ViewModels
+{
+    public class EditProducerView
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        
+        public int Phone { get; set; }
+
+        public string Email { get; set; }
+
+        public string WebSite { get; set; }
+    }
+}
