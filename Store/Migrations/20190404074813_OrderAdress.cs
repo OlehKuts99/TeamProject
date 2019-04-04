7fdae358d49dba8cc969854fdb8d2cf78ef36de1
Oleh Kuts <oleh.kuts99@gmail.com>
2019-04-04 11:55:09 +0300

    Added new fields to order model : "EndPointCity", "EndPointStreet"

---
+++ Store/Migrations/20190404074813_OrderAdress.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Store.Migrations
+{
+    public partial class OrderAdress : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "EndPointCity",
+                table: "Orders",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "EndPointStreet",
+                table: "Orders",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndPointCity",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "EndPointStreet",
+                table: "Orders");
+        }
+    }
+}
