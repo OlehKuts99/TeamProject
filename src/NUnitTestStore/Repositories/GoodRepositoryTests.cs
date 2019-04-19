d6deb9bdf4358107fb02e7749fcdee13190f7fcc
Andriy Galas <halasandreyko@gmail.com>
2019-04-19 13:01:20 +0300

    Added test to GoodRepositoryTests

--- src/NUnitTestStore/Repositories/GoodRepositoryTests.cs
+++ src/NUnitTestStore/Repositories/GoodRepositoryTests.cs
@@ -1,21 +1,13 @@
 ﻿using DAL.Models;
 using NUnit.Framework;
-using Store.Controllers;
-using NSubstitute;
-using Store.ViewModels;
-using System.Collections.Generic;
-using System;
 using System.Linq;
 using System.Threading.Tasks;
-using DAL.Classes;
 using DAL.Classes.UnitOfWork;
 using DAL.Classes.UnitOfWork.Classes;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
-using Store.Helpers;
 using Microsoft.EntityFrameworkCore;
 using Moq;
 using DAL.Classes.UnitOfWork.Interfaces;
+using System.Collections.Generic;
 
 namespace NUnitTestStore.Repositories
 {
@@ -28,7 +20,7 @@ namespace NUnitTestStore.Repositories
         public void Setup()
         {
             options= new DbContextOptionsBuilder<AppDbContext>()
-               .UseInMemoryDatabase(databaseName: "Add_writes_to_database").Options;
+               .UseInMemoryDatabase(databaseName: "InMemoryDatabase").Options;
             repoMock = new Mock<IRepository<Good>>();
         }
         
@@ -37,16 +29,18 @@ namespace NUnitTestStore.Repositories
         public async Task CreateTest()
         {
             //Arrange
-            var good = new Good { Id = 1, Name = "Test"};
             using (var context = new AppDbContext(options))
             {
+                var good = new Good { Id = 1, Name = "Test" };
                 var repo = new GoodRepository(context);
 
                 //Act
                 await repo.Create(good);
                 var expectedResult = 1;
+                var actualResult = context.Goods.Local.Count();
+
                 //Assert
-                Assert.AreEqual(expectedResult, context.Goods.Local.Count());
+                Assert.AreEqual(expectedResult, actualResult);
             }
         }
 
@@ -54,21 +48,124 @@ namespace NUnitTestStore.Repositories
         public async Task DeleteTest()
         {
             //Arrange
-            var good = new Good { Id = 1, Name = "Test" };
             using (var context = new AppDbContext(options))
             {
-                UnitOfWork unitOfWork = new UnitOfWork(context);
+                var good = new Good { Id = 1, Name = "Test" };
                 var repo = new GoodRepository(context);
 
                 //Act
                 var expectedResult = 0;
                 context.Goods.Add(good);
-                var countAfterAdding = context.Goods.Local.Count();
                 await repo.Delete(good.Id);
+                var actualResult = context.Goods.Local.Count();
 
                 //Assert
-                Assert.AreEqual(expectedResult, context.Goods.Local.Count());
-                Assert.AreEqual(1, countAfterAdding);
+                Assert.AreEqual(expectedResult, actualResult);
+            }
+        }
+
+        [Test]
+        public async Task GetTest()
+        {
+            //Arrange
+            using (var context = new AppDbContext(options))
+            {
+                var good = new Good { Id = 1, Name = "Test" };
+                var repo = new GoodRepository(context);
+
+                //Act
+                var expectedResult = good;
+                context.Goods.Add(good);
+                var result = await repo.Get(good.Id);
+
+                //Assert
+                Assert.AreEqual(expectedResult, result);
+            }
+        }
+
+        [Test]
+        public void GetAllTest()
+        {
+            //Arrange
+            using (var context = new AppDbContext(options))
+            {
+                var goods = new List<Good> { new Good { Id = 1, }, new Good { Id = 2 }, new Good { Id = 3 } };
+                var repo = new GoodRepository(context);
+
+                //Act
+                foreach (var good in goods)
+                {
+                    context.Goods.Add(good);
+                }
+                var result = repo.GetAll();
+
+                //Assert
+                Assert.AreEqual(context.Goods, result);
+            }
+        }
+
+        [Test]
+        public void UpdateTest()
+        {
+            //Arrange
+            using (var context = new AppDbContext(options))
+            {
+                var good = new Good { Id = 1, Name = "Test" };
+                var repo = new GoodRepository(context);
+
+                //Act
+                repo.Update(good);
+                var expectedResult = EntityState.Modified;
+                var actualResult = context.Entry(good).State;
+
+                //Assert
+                Assert.AreEqual(expectedResult, actualResult);
+            }
+        }
+
+        [Test]
+        public async Task AddGoodToStorageTest()
+        {
+            //Arrange
+            using (var context = new AppDbContext(options))
+            {
+                var storages = new List<Storage> { new Storage { Id = 1, }, new Storage { Id = 2 },
+                    new Storage { Id = 3 } };
+                var good = new Good { Id = 1, Name = "Test" };
+                var repo = new GoodRepository(context);
+
+                //Act
+                await repo.Create(good);
+                await repo.AddGoodToStorage(good.Id, storages);
+                var actualResult = context.GoodStorage.Local.Count();
+                var expectedResult = 3;
+
+                //Assert
+                Assert.AreEqual(expectedResult, actualResult);
+            }
+        }
+        
+
+        [Test]
+        public async Task AddReview()
+        {
+            //Arrange
+            using (var context = new AppDbContext(options))
+            {
+                var review = new GoodReview {  Id = 1, GoodId = 1 };
+                var good = new Good { Id = 1, Name = "Test" };
+                var repo = new GoodRepository(context);
+
+                //Act
+                await repo.Create(good);
+                context.Reviews.Add(review);
+                await repo.AddReview(review, good);
+                var actualResult = context.Reviews.Find(review.Id);
+                var expectedResult = review;
+
+                //Assert
+                Assert.AreEqual(expectedResult, actualResult);
+                Assert.AreEqual(good.Reviews, new List<GoodReview> { review });
             }
         }
     }
